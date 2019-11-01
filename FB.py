afa946e50469942b4bff07c4e809fe580bd3e717
Farhaan Shaikh <farhaan.shaikh@pure-systems.com>
2019-11-01 12:10:35 +0100

    firct commit

---
+++ FB.py
@@ -0,0 +1,55 @@
+'''
+Created on Nov 1, 2019
+
+@author: farhaan
+'''
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def build_chrome_options():
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.accept_untrusted_certs = True
+        chrome_options.assume_untrusted_cert_issuer = True
+        # chrome configuration
+        # More: https://github.com/SeleniumHQ/docker-selenium/issues/89
+        # And: https://github.com/SeleniumHQ/docker-selenium/issues/87
+#         chrome_options.add_argument("--no-sandbox")
+#         chrome_options.add_argument("--disable-impl-side-painting")
+#         chrome_options.add_argument("--disable-setuid-sandbox")
+#         chrome_options.add_argument("--disable-seccomp-filter-sandbox")
+#         chrome_options.add_argument("--disable-breakpad")
+#         chrome_options.add_argument("--disable-client-side-phishing-detection")
+#         chrome_options.add_argument("--disable-cast")
+#         chrome_options.add_argument("--disable-cast-streaming-hw-encoding")
+#         chrome_options.add_argument("--disable-cloud-import")
+#         chrome_options.add_argument("--disable-popup-blocking")
+#         chrome_options.add_argument("--ignore-certificate-errors")
+#         chrome_options.add_argument("--disable-session-crashed-bubble")
+#         chrome_options.add_argument("--disable-ipv6")
+#         chrome_options.add_argument("--allow-http-screen-capture")
+        chrome_options.add_argument("--start-maximized")
+
+        return chrome_options 
+
+def main():
+    
+    options = build_chrome_options()
+    driver = webdriver.Chrome('usr/bin/chromedriver',options=options)
+    
+    user_name = "uname"
+    password = "pwd"
+#     driver = webdriver.Chrome('usr/bin/chromedriver')
+    driver.get("https://www.facebook.com")
+#     driver.manage().window().maximize()
+    element = driver.find_element_by_id("email")
+    element.send_keys(user_name)
+    element = driver.find_element_by_id("pass")
+    element.send_keys(password)
+    element.send_keys(Keys.RETURN)
+    element.close()
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,15 @@
+###Selenium with Python
+
+selenium example in python to open a webpage and fill a from (here facebok)
+
+With chrome driver in folder "./usr/bin"
+options for the chrome driver are written and commented out in the function - build_chrome_options()
+
+requires selenium 
+```{r, engine='python', count_lines}
+pip install selenium
+```
+
+run the script - FB.py
+
+python version - v3.8
